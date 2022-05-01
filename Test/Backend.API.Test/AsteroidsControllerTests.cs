e894f638892759155b65766206a532bdeec4dc58
Alberto J <alberto.carracedo@solid-stack.com>
2022-05-01 17:47:51 +0200

    Test project is added

---
+++ Test/Backend.API.Test/AsteroidsControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backend.API.Test
+{
+    [TestClass]
+    public class AsteroidsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

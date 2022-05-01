97fdde28987156e8ad76f96c84623928a7c85541
Alberto J <alberto.carracedo@solid-stack.com>
2022-05-01 16:25:05 +0200

    Agregar archivos de proyecto.

---
+++ Backend.Constant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend.Constant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
